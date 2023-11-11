487c2d11a4b7f96972873d40ecf598301f5f1e97
rashakanakri <148903870+rashakanakri@users.noreply.github.com>
2023-11-11 22:16:44 +0300

    add methods topic to our intro

---
+++ dart_intro/bin/methods.dart
@@ -0,0 +1,76 @@
+void main (){
+
+
+// params and return 
+// void
+// data type return type
+
+// void methodsName(){
+// code
+// }
+
+void printName (){
+
+print('rasha');
+print('mousa');
+print('kanakri');
+
+}
+ printName();
+
+//  method to calculate the sum of 2 numbers
+
+
+// void sum (var a,var b){
+// var z=a+b;
+// return print(z);
+// }
+// sum (5,6);
+
+void sum ({var a,var b,var c,var d, var e}){
+ var z=a+b;
+ return print(z);
+ }
+ sum (
+a:5,
+b:4,
+c:3,
+d:8,
+e:1,
+
+ );
+
+// method at return string
+
+
+// void fullname (){
+// print('rasha mousa kanakri');
+
+// }
+// fullname();
+
+ void  name(String firstname,String lastname){
+  String fullname=firstname +" "+lastname;
+  print(fullname);
+}
+name("rasha","kanakri");
+
+
+
+// methods at int string
+void fruits (int strawberry ,int apples,int banana){
+
+  int pricefruits=strawberry+apples+banana;
+  print(pricefruits);
+
+}
+fruits(3,1,2);
+
+  
+
+
+ 
+
+
+
+}
